2ef14a639fa01f5f69362b318a8d7f4ea1ea3e42
LeHai1004 <lehai191006@gmail.com>
2026-03-10 16:31:12 +0700

    tinh_tong

---
+++ Bai16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai16
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Bài 16 - Tính tổng | SV: Lê Hoàng Hải - 2415053122213 - 225LTC#01");
+
+            List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+
+            Console.WriteLine("Danh sách ban đầu: " + string.Join(" ", numbers));
+
+            var result = numbers.Sum();
+
+            Console.WriteLine("\nTổng các số trong danh sách: " + result);
+        }
+    }
+}
