6835d55c320747819994effa6affaa8cefb4703a
LeHai1004 <lehai191006@gmail.com>
2026-03-10 14:40:36 +0700

    Add project files.

---
+++ Bai1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Bài 1 - Lọc số chẵn | SV: Lê Hoàng Hải - 2415053122213 - 225LTC#01");
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            Console.WriteLine("Danh sách ban đầu: " + string.Join(", ", numbers));
+
+            var result = numbers.Where(n => n % 2 == 0);
+
+            Console.WriteLine("Kết quả lọc số chẵn: " + string.Join(", ", result));
+        }
+    }
+}
